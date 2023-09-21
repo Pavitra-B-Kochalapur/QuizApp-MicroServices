cf203bd7941995f31129153a45f3e8c5fadbb559
Pavitra-B-Kochalapur <bkpavitra99@gmail.com>
2023-09-22 00:02:19 +0530

    First Commit
    Uploading API-Gateway

---
+++ API-Gateway/src/main/java/com/pavitrabk/apigateway/apigatewayApplication.java
@@ -0,0 +1,13 @@
+package com.pavitrabk.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class apigatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(apigatewayApplication.class, args);
+	}
+
+}
